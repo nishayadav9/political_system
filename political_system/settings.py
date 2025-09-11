c09d2df12110544b658b6d7b4583774ce4edafc8
nishayadav9 <csdt.it@gmail.com>
2025-09-11 12:11:59 +0530

    Update ALLOWED_HOSTS for Render deployment

--- political_system/settings.py
+++ political_system/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!s39i)cn0a6l$gkqazhm^q%swdb!61by#^1a54za=*polhxy=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ["political-system-clean-11.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["political_system-3.onrender.com", "127.0.0.1"]
 
 
 
